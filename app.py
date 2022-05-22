f98a6505b3800303e749bf3e2c1e2bfae9df6e24
smithsh14 <smithsh14@ecu.edu>
2022-05-21 21:05:48 -0400

    Initial challenge commit

--- app.py
+++ app.py
@@ -1,40 +1,27 @@
-import datetime as dt
 import numpy as np
-import pandas as pd
+import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
-
 engine = create_engine("sqlite:///hawaii.sqlite")
-
 Base = automap_base()
-
 Base.prepare(engine, reflect=True)
-
 Measurement = Base.classes.measurement
 Station = Base.classes.station
-
-session = Session(engine) 
-
+session = Session(engine)
 app = Flask(__name__)
+start_date=dt.datetime(2016,8,23)
+end_date=dt.datetime(2017,8,23)
 @app.route("/")
 def welcome():
-    return(
-    '''
-    Welcome to the Climate Analysis API!
-    Available Routes:
-    /api/v1.0/precipitation
-    /api/v1.0/stations
-    /api/v1.0/tobs
-    /api/v1.0/temp/start/end
-    ''')
-
-@app.route("/api/v1.0/precipitation")
-def precipitation():
-   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-   precipitation = session.query(Measurement.date, Measurement.prcp).\
-    filter(Measurement.date >= prev_year).all()
-   precip = {date: prcp for date, prcp in precipitation}
-   return jsonify(precip)
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
+    )
